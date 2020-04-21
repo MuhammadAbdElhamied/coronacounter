8e83668d3aed9cd0bec262be033a2225a0e2f27f
Muhammad A. Sharaqi <MuhammadSharaqi@gmail.com>
2020-04-21 14:45:22 +0200

    add percentages and some words

--- routes/index.js
+++ routes/index.js
@@ -11,12 +11,16 @@ router.get('/', function(req, res, next) {
     res.render('index',
    { 
      title: 'Coronacounter',
-     __deaths: deaths,
+     mainJS: 'main.js',
+     __deaths: deaths + ' ≈ ' + Math.round(((deaths / confirmed)*100)) + '%',
      __confirmed: confirmed,
-     __recovered: recovered,
+     __recovered: recovered + ' ≈ ' + Math.round(((recovered / confirmed)*100)) + '%',
+
+      __DPRSNT: 'The percentage of total cases: ' + ((deaths / confirmed)*100) + ' %',
+      __RPRSNT: 'The percentage of total cases: ' + ((recovered / confirmed)*100) + ' %',
+     __confiremed_persent: 'Assuming that the number of infected cases is: ' + '100 %',
      //country: req.body.countries
     });
-
 });
 
 
@@ -24,5 +28,6 @@ router.get('/', function(req, res, next) {
 var deaths = fetch(__apiUrl).then(rees => rees.json()).then(json => deaths = (json.deaths.value));
 var confirmed = fetch(__apiUrl).then(rees => rees.json()).then(json => confirmed = (json.confirmed.value))
 var recovered = fetch(__apiUrl).then(rees => rees.json()).then(json => recovered = (json.recovered.value))
+
 
 module.exports = router;
